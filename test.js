2c901f06ce32439606f08f0c4f4a85bb00ca727f
Tsui Ka Po <kptsui@ust.hk>
2017-02-20 13:45:51 +0800

    Added test function: closestBeaconAsPoint

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 
   testPoints(test_data, 0);
 
+  //closestBeaconAsPoint(test_data);
 });
 
 /*
@@ -25,6 +26,39 @@ function testPoints(data, i){
       testPoints(data, i + 1);
     }
   }, 400);
+}
+
+function closestBeaconAsPoint(test_data_object){
+  var min_rssi = Number.MAX_SAFE_INTEGER;
+  var closestBeacon;
+  for(var i = 0; i < test_data_object.beacons.length; i++){
+    var beacon = test_data_object.beacons[i];
+    if(beacon.rssi < min_rssi){
+      min_rssi = beacon.rssi;
+      closestBeacon = beacon;
+    }
+  }
+  var result = {
+    "x": 0,
+    "y": 0
+  };
+  for(var i = 0; i < deployed_beacons.length; i++){
+    if(closestBeacon.bid == deployed_beacons[i].bid){
+      result.x = deployed_beacons[i].x;
+      result.y = deployed_beacons[i].y;
+      break;
+    }
+  }
+
+	// remove previous marker
+  console.log("remove previous marker");
+  if(marker != null){
+    map.removeLayer(marker);
+  }
+
+	// re-print the marker
+	marker = L.marker([result.y, result.x], {icon: markerIcon});
+  marker.addTo(map).bindPopup('You are around here!'); // y, x in pixel
 }
 
 /**
